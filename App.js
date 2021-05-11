6da0d65764e20754c7a08acc5c176a0fc05a6c5f
JulienParis26 <71380678+JulienParis26@users.noreply.github.com>
2021-05-11 15:44:58 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+
+      {/* {Cards} */}
+      <View style={styles.cards}>
+        <Text style={styles.sectionTitle}>Today's Card</Text>
+
+        <View style={styles.items}>
+           {/* {This is where the card will go} */}
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#E8EAED',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  cards: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  },
+  items: {},
+});
