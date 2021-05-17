ac215a2e527eb3fa786a5a754063446c00435db6
aymericthomine <thomine.aymeric@gmail.com>
2021-05-17 14:55:01 +0200

    Add server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const urljoin = require('url-join');
-
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 app.get('/restaurants', (req, res) => {
     res.sendFile(path.join(__dirname, '../server', 'restaurants.json'));
 });
-
 app.listen(port, () => {
    console.log('Server is up!');
 });
