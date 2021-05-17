a76ac253081afd31be5094b5f655e8bbbc4fddb8
aymericthomine <thomine.aymeric@gmail.com>
2021-05-17 12:55:47 +0200

    Add server.js

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../server', 'maping.json'));
+app.get('/restaurants', (req, res) => {
+    res.sendFile(path.join(__dirname, '../server', 'restaurants.json'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
