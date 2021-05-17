f2deb684483637b83de9872611cc89487bde34c8
aymericthomine <thomine.aymeric@gmail.com>
2021-05-17 14:50:53 +0200

    Add server.js

--- server/server.js
+++ server/server.js
@@ -9,10 +9,6 @@ app.use(express.static(publicPath));
 app.get('/restaurants', (req, res) => {
     res.sendFile(path.join(__dirname, '../server', 'restaurants.json'));
 });
-//app.use('/restaurants/:id', function (req, res, ) {
-//    var fullUrl = urljoin('http://restaurant-le-k.fr/wp-content/uploads/2020/05/Menu-le-K4.jpg');
-//    console.log(fullUrl);
-//  });
 
 app.listen(port, () => {
    console.log('Server is up!');
